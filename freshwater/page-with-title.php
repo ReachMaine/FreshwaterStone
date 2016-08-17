6ebafffd0aebd8409fb319014d487f499e15c9f5
Linda Speight <linda@reachmaine.com>
2016-08-17 15:24:41 -0400

    Modify page-with-title page template
    and style the page title.

--- freshwater/page-with-title.php
+++ freshwater/page-with-title.php
@@ -1,36 +1,28 @@
 <?php
 /*
  *
- * * Template Name: Page - Full width.
+ * Template Name: Page With Title
  *
-*/
-/* mods 
-*     16Aug16 zig - remove page-breadcrumb & page title 
 */
 get_header(); 
 global $be_themes_data; 
 while(have_posts()): the_post();
 	$be_pb_class = 'page-builder';
 	$be_pb_disabled = get_post_meta( $post->ID, '_be_pb_disable', true );
-	if( !isset($be_pb_disabled) || true === $be_pb_disabled || 'yes' == $be_pb_disabled || !isset( $be_themes_data['enable_pb'] ) || 0 == $be_themes_data['enable_pb'] ) {
+	if( true === $be_pb_disabled || 'yes' == $be_pb_disabled || !isset( $be_themes_data['enable_pb'] ) || 0 == $be_themes_data['enable_pb'] ) {
 		$be_pb_class = 'be-wrap no-page-builder';
-		$be_pb_class = 'no-page-builder';
-		//get_template_part( 'page-breadcrumb' );
-	} ?>
-	<div id="content" class="no-sidebar-page fws-full-width">
-		
+	}
+	//get_template_part( 'page-breadcrumb' );
+	?>
+	<section id="content" class="no-sidebar-page fws-page-with-title">
+
 		<div id="content-wrap" class="<?php echo $be_pb_class; ?>">
+			<div class="page-title-container" >
+				<h1 class="page-title fws-center"><?php the_title();  ?></h1>
+			</div>
 			<section id="page-content">
 				<div class="clearfix">
-					<?php 
-						if ( post_password_required() ) {
-	       	 				$content  = get_the_password_form();
-
-	       	 			    echo '<div class="be-wrap clearfix be-section-pad">'.$content.'</div>';
-	       	 			} else {
-							the_content();
-						}
-					?>
+					<?php the_content(); ?>
 				</div> <!--  End Page Content -->
 				<?php if( isset($be_themes_data['comments_on_page']) && $be_themes_data['comments_on_page'] == 1 ) : ?>
 					<div class="be-themes-comments be-row be-wrap">
@@ -39,6 +31,6 @@ while(have_posts()): the_post();
 				<?php endif; ?>
 			</section>
 		</div>
-	</div>	<?php 
+	</section>	<?php 
 endwhile;
 get_footer(); ?>
